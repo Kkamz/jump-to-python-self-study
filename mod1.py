d27c93adf112f98f88b96dd1c90660a33ee0f3c2
Kkamz <alal889@nate.com>
2021-04-26 16:20:35 +0900

    jumptopython

---
+++ mod1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def sum(a,b):
+    return a+b
+
+def safe_sum(a,b):
+    if type(a) != type(b):
+        print("더할수 있는 것이 아닙니다.")
+        return
+    else:
+        result = sum(a,b)
+    return result
+
+
+# In[ ]:
+
+
+
+
